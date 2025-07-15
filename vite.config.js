ec884765062108c87b0d66bca5dcc80a26c8bce2
sasi <sasidharkanigolla44@gmail.com>
2025-07-15 16:51:57 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/", // 👈 Add this line
+  base: "/Portifolio-website/", // ✅ MUST MATCH YOUR GITHUB REPO NAME
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
